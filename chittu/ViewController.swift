c06f214a8d5c355c2da012a8c26dc039ea16ae11
Raja Vijaya Saradhi <rvrajavijay@gmail.com>
2018-04-15 01:04:21 +0530

    Added jellyfish.dae

--- chittu/ViewController.swift
+++ chittu/ViewController.swift
@@ -7,19 +7,55 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints,ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configuration)
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self,action: #selector(handleTap))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func play(_ sender: Any) {
+        self.addNode()
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+    }
+    
+    func addNode(){
+        let jellyFishScene = SCNScene(named: "art.scnassets/Jellyfish.scn")
+        let jellyfishNode = jellyFishScene?.rootNode.childNode(withName: "Jellyfish", recursively: false)
+        jellyfishNode?.position = SCNVector3(0,0,-1)
+        self.sceneView.scene.rootNode.addChildNode(jellyfishNode!)
+    }
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        let sceneViewTappedOn = sender.view as! SCNView
+        let touchCoordinates = sender.location(in: sceneViewTappedOn)
+        let hitTest = sceneViewTappedOn.hitTest(touchCoordinates)
+        if hitTest.isEmpty{
+            print("Didn't touch")
+        } else {
+            let results = hitTest.first!
+            let geometry = results.node.geometry
+            print(geometry)
+            
+        }
+    }
+    
+    
 
 }
 
